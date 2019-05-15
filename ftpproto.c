c1446671d1288bb415490e76fa29a344f3f3bfd4
songwei163 <songweicpp@163.com>
2019-05-15 23:04:03 +0800

    验证密码,切换用户

--- ftpproto.c
+++ ftpproto.c
@@ -6,8 +6,90 @@
 #include "ftpcodes.h"
 
 void ftp_reply (session_t *sess, int status, const char *text);
+
 static void do_user (session_t *sess);
 static void do_pass (session_t *sess);
+static void do_cwd (session_t *sess);
+static void do_cdup (session_t *sess);
+static void do_quit (session_t *sess);
+static void do_port (session_t *sess);
+static void do_pasv (session_t *sess);
+static void do_type (session_t *sess);
+static void do_stru (session_t *sess);
+static void do_mode (session_t *sess);
+static void do_retr (session_t *sess);
+static void do_stor (session_t *sess);
+static void do_appe (session_t *sess);
+static void do_list (session_t *sess);
+static void do_nlst (session_t *sess);
+static void do_rest (session_t *sess);
+static void do_abor (session_t *sess);
+static void do_pwd (session_t *sess);
+static void do_mkd (session_t *sess);
+static void do_rmd (session_t *sess);
+static void do_dele (session_t *sess);
+static void do_rnfr (session_t *sess);
+static void do_rnto (session_t *sess);
+static void do_site (session_t *sess);
+static void do_syst (session_t *sess);
+static void do_feat (session_t *sess);
+static void do_size (session_t *sess);
+static void do_stat (session_t *sess);
+static void do_noop (session_t *sess);
+static void do_help (session_t *sess);
+
+typedef struct ftpcmd {
+    const char *cmd;
+    void (*cmd_handler) (session_t *sess);
+} ftpcmd_t;
+
+static ftpcmd_t ctrl_cmds[] = {
+    /* 访问控制命令 */
+    {"USER",                 do_user},
+    {"PASS",                 do_pass},
+    {"CWD",                  do_cwd},
+    {"XCWD",                 do_cwd},
+    {"CDUP",                 do_cdup},
+    {"XCUP",                 do_cdup},
+    {"QUIT",                 do_quit},
+    {"ACCT",               NULL},
+    {"SMNT",               NULL},
+    {"REIN",               NULL},
+    /* 传输参数命令 */
+    {"PORT",                 do_port},
+    {"PASV",                 do_pasv},
+    {"TYPE",                 do_type},
+    {"STRU",    /*do_stru*/NULL},
+    {"MODE",    /*do_mode*/NULL},
+
+    /* 服务命令 */
+    {"RETR",                 do_retr},
+    {"STOR",                 do_stor},
+    {"APPE",                 do_appe},
+    {"LIST",                 do_list},
+    {"NLST",                 do_nlst},
+    {"REST",                 do_rest},
+    {"ABOR",                 do_abor},
+    {"\377\364\377\362ABOR", do_abor},
+    {"PWD",                  do_pwd},
+    {"XPWD",                 do_pwd},
+    {"MKD",                  do_mkd},
+    {"XMKD",                 do_mkd},
+    {"RMD",                  do_rmd},
+    {"XRMD",                 do_rmd},
+    {"DELE",                 do_dele},
+    {"RNFR",                 do_rnfr},
+    {"RNTO",                 do_rnto},
+    {"SITE",                 do_site},
+    {"SYST",                 do_syst},
+    {"FEAT",                 do_feat},
+    {"SIZE",                 do_size},
+    {"STAT",                 do_stat},
+    {"NOOP",                 do_noop},
+    {"HELP",                 do_help},
+    {"STOU",               NULL},
+    {"ALLO",               NULL}
+};
 
 void handle_child (session_t *sess)
 {
@@ -40,6 +122,8 @@ void handle_child (session_t *sess)
       //将命令转换为大写
       str_upper (sess->cmd);
       //处理FTP命令
+      //命令映射
+      /*
       if (strcmp ("USER", sess->cmd) == 0)
         {
           do_user (sess);
@@ -47,6 +131,29 @@ void handle_child (session_t *sess)
       else if (strcmp ("PASS", sess->cmd) == 0)
         {
           do_pass (sess);
+        }
+        */
+      int i = 0;
+      int size = sizeof (ctrl_cmds) / sizeof (ctrl_cmds[0]);
+      for (i = 0; i < size; ++i)
+        {
+          if (strcmp (ctrl_cmds[i].cmd, sess->cmd) == 0)
+            {
+              if (ctrl_cmds[i].cmd_handler != NULL)
+                {
+                  ctrl_cmds[i].cmd_handler (sess);
+                }
+              else
+                {
+                  ftp_reply (sess, FTP_COMMANDNOTIMPL, "Unimplement command.");
+                }
+
+              break;
+            }
+        }
+      if (i == size)
+        {
+          ftp_reply (sess, FTP_BADCMD, "Unkonw command.");
         }
     }
 }
@@ -112,3 +219,62 @@ static void do_pass (session_t *sess)
   ftp_reply (sess, FTP_LOGINOK, "Login successful.");
 
 }
+
+static void do_cwd (session_t *sess)
+{}
+static void do_cdup (session_t *sess)
+{}
+static void do_quit (session_t *sess)
+{}
+static void do_port (session_t *sess)
+{}
+static void do_pasv (session_t *sess)
+{}
+static void do_type (session_t *sess)
+{}
+static void do_stru (session_t *sess)
+{}
+static void do_mode (session_t *sess)
+{}
+static void do_retr (session_t *sess)
+{}
+static void do_stor (session_t *sess)
+{}
+static void do_appe (session_t *sess)
+{}
+static void do_list (session_t *sess)
+{}
+static void do_nlst (session_t *sess)
+{}
+static void do_rest (session_t *sess)
+{}
+static void do_abor (session_t *sess)
+{}
+static void do_pwd (session_t *sess)
+{}
+static void do_mkd (session_t *sess)
+{}
+static void do_rmd (session_t *sess)
+{}
+static void do_dele (session_t *sess)
+{}
+static void do_rnfr (session_t *sess)
+{}
+static void do_rnto (session_t *sess)
+{}
+static void do_site (session_t *sess)
+{}
+static void do_syst (session_t *sess)
+{
+  ftp_reply (sess,FTP_SYSTOK,"UNIX Type: L8");
+}
+static void do_feat (session_t *sess)
+{}
+static void do_size (session_t *sess)
+{}
+static void do_stat (session_t *sess)
+{}
+static void do_noop (session_t *sess)
+{}
+static void do_help (session_t *sess)
+{}
