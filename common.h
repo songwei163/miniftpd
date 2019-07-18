08bdaadc44abb63997a5bd48850169c65a70a7e0
songwei163 <songweicpp@163.com>
2019-07-18 15:24:27 +0800

    new

--- common.h
+++ common.h
@@ -6,29 +6,37 @@
 #define _COMMON_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <time.h>
 #include <ctype.h>
 #include <errno.h>
+
 #include <unistd.h>
 #include <fcntl.h>
-#include <dirent.h>
-#include <sys/stat.h>
+#include<signal.h>
 #include <sys/time.h>
-#include <time.h>
 #include <libgen.h>
+#include <net/if.h>
+#include <net/if_arp.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+
+//shdows password
 #include <pwd.h>
 #include <shadow.h>
 #include <crypt.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <net/if_arp.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+
+//socket
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+
+//readdir
+#include<sys/stat.h>
+#include<dirent.h>
 
 #define ERR_EXIT(err_str) do { perror(err_str);  \
         exit(EXIT_FAILURE); } while(0)
